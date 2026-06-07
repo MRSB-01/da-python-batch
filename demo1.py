14ba80d6d5c3e23e006efa2913ed481d60b3ade9
MR.SB <157207925+MRSB-01@users.noreply.github.com>
2026-06-07 14:39:30 +0530

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,22 @@
+# str = "A"
+# print(ord(str))
+
+# a = 65
+# print(chr(a))
+
+# str = "Sachin"
+# print(str[1])
+# print(str[-1])
+
+# Slicing
+
+# str = "Sachin Badgujar"
+# print(str[0:6:1])
+# print(str[7::])
+
+name = "Sachin"
+age = 21
+# print('Hello my name is',name, 'and my age is ',21) #regular string
+
+# print(f"Hello my name is {name}, and my age is {age}") #formated string
+
